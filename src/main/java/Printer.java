dcf66f25223647eba14549d80f09de12df7e134d
Felix Müller <felixrelleum@googlemail.com>
2022-02-02 15:34:17 +0100

    Initial commit

---
+++ src/main/java/Printer.java
@@ -0,0 +1,6 @@
+public class Printer {
+
+    public static void print(String str) {
+        System.out.println("Printing from Printer: " + str);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module jitpack.test.library.main {
+//    exports jitpack.test.library.main;
+}
